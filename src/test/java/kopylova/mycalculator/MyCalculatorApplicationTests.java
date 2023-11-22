b5fe0054697b68be08d5a0865f9c5f573a918be6
Anastasiya <agropina_90@mail.ru>
2023-11-22 14:49:01 +0300

    Initial commit

---
+++ src/test/java/kopylova/mycalculator/MyCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package kopylova.mycalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
