69a61bafc732d7d5feb55393ca1a9d6aac9949f3
Anastasiya <agropina_90@mail.ru>
2023-11-22 15:10:01 +0300

    add: написала контроллер

--- src/main/java/kopylova/mycalculator/controllers/CalculatorController.java
+++ src/main/java/kopylova/mycalculator/controllers/CalculatorController.java
@@ -1,7 +1,32 @@
 package kopylova.mycalculator.controllers;
 
+import jakarta.validation.constraints.Pattern;
+import kopylova.mycalculator.services.CalculatorService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/calculator")
+@Validated
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class CalculatorController {
+
+    CalculatorService service;
+
+    @GetMapping
+    public String calculator(
+            @RequestParam Long numberFirst,
+            @RequestParam @Pattern(regexp = "\\+-/\\*") String sign,
+            @RequestParam Long numberSecond
+    ) {
+        return service.calculator(numberFirst, sign, numberSecond);
+    }
+
 }
--- src/main/java/kopylova/mycalculator/services/CalculatorService.java
+++ src/main/java/kopylova/mycalculator/services/CalculatorService.java
@@ -4,4 +4,12 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CalculatorService {
+    public String calculator(Long numberFirst, String sign, Long numberSecond) {
+
+
+
+
+        return "";
+
+    }
 }
